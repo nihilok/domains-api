17c46e2ca97baebd2f937f1ff99dd104b051b8b6
nihilok <t0k3n8l4ck@gmail.com>
2021-01-04 12:23:23 +0000

    simulate change in IP function

--- domains_api/__main__.py
+++ domains_api/__main__.py
@@ -1,37 +1,15 @@
 import sys
 
-from . import User, IPChanger
-
-
-user = User()
-
-
-def change_previous_ip():
-    user.previous_ip = '42'
-    user.save_user()
-
-
-def read_api_auth_details():
-    keep = gkeep_login()
-    g_notes = keep.find(query=user.domain)
-    username, password, domain = None, None, None
-    try:
-        for note in g_notes:
-            note = str(note).split('\n')
-            domain = note[0]
-            username = note[1]
-            password = note[2]
-        return username, password, domain
-    except IndexError:
-        return username, password, domain
-
-
-def auto_create_api_profile():
-    user.DNS_username, user.DNS_password, user.domain = read_api_auth_details()
-    user.save_user()
+from domains_api import User, IPChanger
 
 
 if __name__ == '__main__':
-    IPChanger(sys.argv[1:])
 
-    from gkeep_funcs import gkeep_login
+    # # Uncomment to simulate a change of IP:
+    # user = User()
+    # def change_previous_ip():
+    #     user.previous_ip = '42'
+    #     user.save_user()
+    # change_previous_ip()
+
+    IPChanger(sys.argv[1:])
