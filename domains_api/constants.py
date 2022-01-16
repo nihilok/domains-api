1fd7362a6156f3aca02b1ee25508ab47cd434ef1
nihilok <mjfullstack@gmail.com>
2022-01-16 16:04:51 +0000

    hotfix for version file not found

--- domains_api/constants.py
+++ domains_api/constants.py
@@ -1,2 +1,7 @@
-with open('version.txt', 'r') as f:
-    __VERSION__ = f.read().strip()
+# import os
+# from pathlib import Path
+#
+# with open(Path(os.path.dirname(__file__)) / 'version.txt', 'r') as f:
+#     __VERSION__ = f.read().strip()
+
+__VERSION__ = '0.2.10.1'
