82b6bb32afe58ec44bc5a51733775dbe64ba31b0
nihilok <mjfullstack@gmail.com>
2022-01-23 20:19:00 +0000

    laptop changes (incomplete)

--- domains_api/ip_changer_v2.py
+++ domains_api/ip_changer_v2.py
@@ -1,8 +1,13 @@
 from dataclasses import dataclass
 from getpass import getpass
-from typing import Optional
+from itertools import cycle
+from typing import Optional, List
+
+from requests import get, post
 
 from domains_api.file_handlers import FileHandlers
+from domains_api.arg_parser import parser
+from domains_api.constants import __VERSION__
 
 
 class UserNotSetupException(Exception):
@@ -21,6 +26,7 @@ class User:
     api_key: str
     api_sec: str
     email_notifications: str
+    BASE_URL = "@domains.google.com/nic/update?hostname="
     gmail_address: Optional[str] = None
     gmail_app_password: Optional[str] = None
     last_ip: Optional[str] = None
@@ -35,12 +41,16 @@ class User:
     def set_notifications(self, n: str):
         self.email_notifications = n
 
+    def set_email(self, email: str, password: str):
+        self.gmail_address = email
+        self.gmail_app_password = password
+
 
 class IPChanger:
 
     fh = FileHandlers()
 
-    def __init__(self, argv: Optional[list[str]] = None, cli: bool = False):
+    def __init__(self, argv: Optional[List[str]] = None):
         self.user: Optional[User] = None
         self.changed: bool = False
         if argv is not None:
@@ -57,29 +67,99 @@ class IPChanger:
 
     @staticmethod
     def get_ip():
-        pass
+        return get('https://api.ipify.com').text
 
     def check_ip(self):
         self.check_user()
-        if self.get_ip() != self.user.last_ip:
-            self.changed = True
+        if ip := self.get_ip() != self.user.last_ip:
+            self.call_api(ip)
 
-    def call_api(self, force=False):
-        if self.changed or force:
-            pass
+    def call_api(self, ip: str):
+        return post(f"{self.user.req_url}&myip={ip}").text
+
+    def parse_api_response(self, response: str):
+        log_msg = "DDNS API response: %s" % response
+        self.fh.log(log_msg, "info")
+
+        # Successful request:
+        if "good" in response:
+            self.user.send_notification(self.current_ip)
+            log_msg = f"IP changed successfully to {self.current_ip}"
+            self.fh.log(log_msg, "info")
+        elif "nochg" in response:
+            log_msg = "No change to IP"
+            self.fh.log(log_msg, "info")
+
+        # Unsuccessful requests:
+        elif response in {"nohost", "notfqdn"}:
+            msg = (
+                "The hostname does not exist, is not a fully qualified domain"
+                " or does not have Dynamic DNS enabled. The script will not be "
+                "able to run until you fix this. See https://support.google.com/domains/answer/6147083?hl=en-CA"
+                " for API documentation"
+            )
+            self.fh.log(msg, "warning")
+            self.user.send_notification(self.current_ip, "error", msg)
+        else:
+            self.fh.log("Could not authenticate with these credentials", "warning")
 
     def check_user(self):
         if self.user is None:
             raise UserNotSetupException
 
-    def parse_args(self, argv: list[str]):
+    def parse_args(self, argv: List[str]):
         opts = parser.parse_args(argv)
-        if {'-l', '--load-user', '-i', '--ip'}.difference(argv):
+        if {"-h", "--help", "-i", "--ip", "-l", "--load_user", "-v", "--version"}.difference(argv):
             self.load_user()
         if opts.ip:
             print(self.get_ip())
-        if opts.load_user:
-            pass
+            return
+        if opts.version:
+            print(__VERSION__)
+            return
+        elif opts.domain:
+            print(self.user.domain)
+            return
+        elif opts.delete_user:
+            self.fh.delete_user()
+            return
+        elif opts.email:
+            self.user.set_email()
+            self.fh.save_user(self.user)
+            self.fh.log("Notification settings changed", "info")
+
+        elif opts.load_user:
+            if input("Are you sure you want to load a new user? [Y/n] ").lower() == "n":
+                return
+            self.user = self.fh.load_user(opts.load_user)
+            self.fh.save_user(self.user)
+            self.fh.log("New user loaded", "info")
+
+        elif opts.notify:
+            n_options = {"Y": "[all changes]", "e": "[errors only]", "n": "[none]"}
+            arg_hash = {"all": "Y", "errors": "e", "off": "n"}
+            options_iter = cycle(n_options.keys())
+            for option in options_iter:
+                if self.user.notifications == option:
+                    break
+            self.user.notifications = arg_hash.get(opts.notify) or next(options_iter)
+            self.fh.save_user(self.user)
+            log_msg = (
+                    "Notification settings changed to %s"
+                    % n_options[self.user.notifications]
+            )
+            self.fh.log(log_msg, "info")
+            if self.user.notifications != 'n' and not self.user.gmail_address:
+                self.fh.log("No email user set, running email set up wizard...", "info")
+                self.user.set_email()
+                self.fh.save_user(self.user)
+
+        elif opts.force:
+            self.fh.log("***Forcing API call***", "info")
+            if opts.force is not True:
+                self.current_ip = opts.force
+                self.fh.log(f"Using IP: {opts.force}", "info")
+            self.call_api(self.current_ip)
 
     def cli_user_setup(self):
         print("Performing user profile setup wizard....")
