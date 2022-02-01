56c4e5792cb1d22f2566731dc5291cbff453a22a
nihilok <mjfullstack@gmail.com>
2022-02-01 22:58:27 +0000

    v0.3.9

--- domains_api/__init__.py
+++ domains_api/__init__.py
@@ -1,6 +1,6 @@
+from domains_api.arg_parser import parser
 from domains_api.constants import __VERSION__, api_responses
 from domains_api.ip_changer import IPChanger
 from domains_api.user import User
-from domains_api.arg_parser import parser
 
 __all__ = ["__VERSION__", "api_responses", "IPChanger", "parser", "User"]
