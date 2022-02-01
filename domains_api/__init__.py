62c0b9da150a0a740ebd7ef6eb415c63d59ef66b
nihilok <mjfullstack@gmail.com>
2022-02-01 23:10:56 +0000

    v0.3.10

--- domains_api/__init__.py
+++ domains_api/__init__.py
@@ -1,6 +1,7 @@
 from domains_api.arg_parser import parser
 from domains_api.constants import __VERSION__, api_responses
-from domains_api.ip_changer import IPChanger
+from domains_api.file_handlers import FileHandlers
 from domains_api.user import User
+from domains_api.ip_changer import IPChanger
 
-__all__ = ["__VERSION__", "api_responses", "IPChanger", "parser", "User"]
+__all__ = ["__VERSION__", "api_responses", "FileHandlers", "IPChanger", "parser", "User"]
